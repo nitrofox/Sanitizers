b3a297b199f3794249078cca99bd8537e4eba042
nitrofox <nitrofox@users.noreply.github.com>
2021-09-07 17:46:58 +0300

    Add files via upload

--- cmake/Sanitizers/SanitizersConfig.cmake
+++ cmake/Sanitizers/SanitizersConfig.cmake
@@ -1,4 +1,4 @@
-set(Sanitizers_VERSION "0.1")
+set(Sanitizers_VERSION "0.2")
 macro(add_to_compile_and_link_flags target name)
     target_compile_options(${target} INTERFACE ${name})
     target_link_options(${target} INTERFACE  ${name})
@@ -49,6 +49,61 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "GN
 else()
     message(WARNING " =(((")
 endif()
+
+
+# target_cxx_sanitizers(${PROJECT_NAME} address)
+# target_cxx_sanitizers(${PROJECT_NAME} undefined)
+# target_cxx_sanitizers(${PROJECT_NAME} memory)
+# target_cxx_sanitizers(${PROJECT_NAME} thread)
+# target_cxx_sanitizers(${PROJECT_NAME} leak)
+
+
+#set(BUILD_ENABLE_SANITIZERS OFF CACHE BOOL BUILD_ENABLE_SANITIZERS)
+#set(BUILD_ENABLE_SANITIZERS_RECOVER OFF CACHE BOOL BUILD_ENABLE_SANITIZERS_RECOVER)
+#if(BUILD_ENABLE_SANITIZERS_UNDEFINED)
+#    target_compile_options(${PROJECT_NAME} PRIVATE  -fsanitize-recover=all)
+#endif()
+set(Clang_address ON)
+set(GNU_address ON)
+
+set(Clang_leak ON)
+set(GNU_leak ON)
+
+set(Clang_thread ON)
+set(GNU_thread ON)
+
+set(Clang_undefined ON)
+set(GNU_undefined ON)
+
+set(Clang_memory ON)
+
+set(Clang_cfi ON)
+
+set(Clang_safe-stack ON)
+
+set(Clang_dataflow ON)
+
+set(GNU_kernel-address ON)
+
+set(GNU_pointer-compare ON)
+
+set(GNU_pointer-subtract ON)
+
+macro(target_cxx_sanitizers target name)
+    if(${CMAKE_CXX_COMPILER_ID}_${name})
+        set(${target}_BUILD_ENABLE_SANITIZERS OFF CACHE BOOL BUILD_ENABLE_SANITIZERS)
+        set(${target}_BUILD_ENABLE_SANITIZERS_${name} OFF CACHE BOOL BUILD_ENABLE_SANITIZERS)
+        if(${target}_BUILD_ENABLE_SANITIZERS_${name} AND ${target}_BUILD_ENABLE_SANITIZERS)
+            target_link_libraries(${target} Sanitizers::${name})
+        endif()
+        set(${target}_BUILD_ENABLE_SANITIZERS_${name}_RECOVER OFF CACHE BOOL BUILD_ENABLE_SANITIZERS_RECOVER)
+        if(${target}_BUILD_ENABLE_SANITIZERS_${name}_RECOVER)
+            target_compile_options(${target} PRIVATE  -fsanitize-recover=${name})
+        endif()
+     else()
+         message(Error: ${CMAKE_CXX_COMPILER_ID} compiler does not support Sanitizer_${name} )
+     endif()
+endmacro()
 #-fsanitize-recover=all
 #export UBSAN_OPTIONS="log_path=UBSAN"
 #export ASAN_OPTIONS="log_path=ASAN"
